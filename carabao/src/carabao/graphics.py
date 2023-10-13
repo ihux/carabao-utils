ea5ff047cc426764f2c86676061ad71ec255e07e
iHux <hugo.pristauz@gmail.com>
2023-10-13 22:18:35 +0200

    add stuff to carabao/graphics

--- carabao/src/carabao/graphics.py
+++ carabao/src/carabao/graphics.py
@@ -4,6 +4,16 @@
 
 import matplotlib.pyplot as plt
 
+class Screen:
+    def __init__(self):
+        self.userdata = None
+        self.figures = {}
+
+class Figure:
+  def __init__(self):
+
+
+
 def demo():
     plt.plot([1, 2, 7, 4])
     plt.ylabel('some numbers')
