c9e8353c833d824661e07be109e1846f62b477a2
iHux <hugo.pristauz@gmail.com>
2023-10-22 12:11:39 +0200

    start Caracow class

--- carabao/src/carabao/__init__.py
+++ carabao/src/carabao/__init__.py
@@ -1,1 +1,14 @@
+# Carabao Utilities:
+# - class Caracow
+# - class Carabull
+# - class Carabao
+# - class Carashit
+#
+# Try:
+#    from carabao import *
+#    o = Carashit()
+#    o.shit
 
+class Carashit:
+    def __init__(self):
+        self.shit = '*** bullshit ***'
