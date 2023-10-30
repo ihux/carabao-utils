42fab13ef7f7cb9fec6d50c09ef431a959c367fc
ihux <hugo.pristauz@bluenetics.com>
2023-10-30 21:18:41 +0100

    use Cell class instead of Rcell in rules

--- carabao/src/carabao/cell.py
+++ carabao/src/carabao/cell.py
@@ -109,6 +109,14 @@ def sat(X):
     def gt0(X): return (X>=0)*X
     return lt1(gt0(X))
 
+#===============================================================================
+# rule 1: excited predictive cells get active
+#===============================================================================
+
+def rule_1(cell,u,c):
+    cell.y = u * cell.x                # excited & predictive cells get active
+    return cell.update(u,c,1)
+
 #=============================================================================
 # class Cell
 #=============================================================================
@@ -320,8 +328,14 @@ class Cell:
     def log(self,txt):
         self.mon.log(self,txt)
 
-    def plot(self,i=None,j=None,v=None,W=None,E=None,u=None,c=None):
-        self.mon.plot(self,i,j,v=v,W=W,E=E,u=u,c=c)
+    def plot(self,i=None,j=None,v=None,W=None,E=None,u=None,c=None,
+	         xlabel=None,head=None,foot=None):
+	    self.input.u = self.input.u if u is None else u
+	    self.input.c = self.input.c if c is None else c
+	    self.mon.plot(self,i,j,v=v,W=W,E=E,u=u,c=c)
+	    if xlabel is not None: self.mon.xlabel(j,xlabel)
+	    if head is not None: self.mon.head(head)
+	    if foot is not None: self.mon.foot(foot)
 
     def set(self,u=None,c=None,x=None,y=None,b=None):
         self.input.u = self.input.u if u is None else u
@@ -331,6 +345,166 @@ class Cell:
         self.b = self.b if b is None else b
         return self
 
+
+#=============================================================================
+# class Cell
+#=============================================================================
+
+class Rcell:
+    """
+    Rcell: class Rcell - modelling HTM cell algorithm
+
+        from carabao.screen import Monitor
+        from carabao.cell import Cell,toy
+        from numpy import ones
+
+        mon = Monitor(m=1,n=3)
+        k,g,K,P,c = toy('cell')
+        cell = Rcell(mon,k,g,K,P)
+        cell.plot(i=0,j=0)    # plot at monitor location i,j
+
+        cell.u = cell.y = cell.x = cell.b = 1
+        cell.plot(i=0,j=1)    # plot at monitor location i,j
+
+        v = [1,0,1,0];  V = ones((2,5));  E = (cell.P >= 0.5)*V
+        cell.plot(0,2,v,E)
+    """
+    def __init__(self,mon,k,g,K,P):
+        self.mon = mon.copy()  # Monitor(mon.screen.m,mon.screen.n,mon.verbose)
+        zero = [0 for i in range(0,P.shape[0])]
+
+            # input variables
+
+        self.input = struct()          # structure for all neuron inputs
+        self.input.u = 0               # basal (feedforwad) input
+        self.input.c = []              # context input
+
+            # output, state variables
+
+        self.y = 0                     # cell output (axon)
+        self.x = 0                     # predictive state
+        self.b = 0                     # burst state
+        self.P = P                     # permanence matrix (state)
+
+            # parameters and auxilliary variables
+
+        self.config(k,g,K)
+        self.x_ = 0                    # auxilliary: x(t+1)
+        self.P_ = self.P               # auxilliary: P(t+1)
+
+    def config(self,k,g,K):
+        self.eta = 0.5                 # synaptic threshold
+        self.theta = 2                 # dendritic threshold
+        self.pdelta = 0.04             # positive learning delata
+        self.ndelta = 0.02             # negative learning delta
+        self.k = k;
+        self.g = g;
+        self.K = K;
+
+    def v(self,c):                     # group output
+        return array([c[k] if k < len(c) else 0
+                      for k in self.g])
+
+    def V(self,c):                    # pre-synaptic signals
+        if c == []: return 0*self.P
+        V = 0*self.K
+        for mu in range(0,self.K.shape[0]):
+            for nu in range(0,self.K.shape[1]):
+                k = self.K[mu,nu]
+                V[mu,nu] = c[k] if k < len(c) else 0
+        return V
+
+    def W(self):
+        return (self.P >= self.eta)*1  # synaptic (binary) weights
+
+    def E(self,c):
+        return self.V(c) * self.W()    # empowerment matrix
+
+    def S(self,c):                     # spike matrix (learning mask)
+        return column(self.s(c)) @ ones(self.P[0,:])
+
+    def L(self,c):                     # learning delta
+        V = self.V(c)                  # pre-synaptic signals
+        S = self.S(c)                  # spike matrix (learning mask)
+        return S*(self.pdelta * V - self.ndelta)
+
+    def s(self,c):                     # spike vector
+        E = self.E(c)
+        s = [(sum(E[mu]) >= self.theta)
+             for mu in range(0,E.shape[0])]
+        return 1*array(s)
+
+    def update(self,u,c,rule):
+        self.set(u=u,c=c)              # store for plot routines
+        c = c.copy();                  # update a copy of the context
+        while len(c) <= self.k: c.append(0)
+        c[self.k] = self.y            # with changed output
+        return c
+
+      # === rule 1: excited predictive cells get active ===
+
+    def rule1(self,u,c): return rule_1(self,u,c)
+
+      # === rule 2: excited neurons in non-predictive groups burst
+
+    def rule2(self,u,c):
+        v = self.v(c)                  # the group's outputs
+        self.b = u * (sum(v) == 0)     # set cell's burst state
+
+           # important: don't change output (and context vector) in this phase
+           # before all cells in the context have determined their burst state
+
+        return self.update(u,c,2)
+
+      # === rule 3: excited bursting neurons get active ===
+
+    def rule3(self,u,c):
+        self.y = u * (self.y or self.b)
+        return self.update(u,c,3)
+
+      # === rule 4: empowered dendritic segments spike ===
+
+    def rule4(self,u,c):
+        V = self.V(c)                            # pre-synaptic signals
+        W = self.W()                             # synaptic weights
+        E = V * W                                # empowerment matrix
+        #s = u * self.s(c)                       # spike vector
+        s = self.s(c)                            # spike vector
+
+        return self.update(u,c,4)
+
+       # === rule 5: spiking dentrites of active cells learn
+
+    def rule5(self,u,c):
+        L = self.L(c)
+        self.P = sat(self.P + self.y * L)        # learning (adapt permanences)
+        return self.update(u,c,5)
+
+      # === rule 6: spiking neurons get always predictive ===
+
+    def rule6(self,u,c):
+        self.x = max(self.s(c))       # dendritic spikes set cells predictive
+        return self.update(u,c,6)
+
+    def log(self,txt):
+        self.mon.log(self,txt)
+
+    def plot(self,i=None,j=None,v=None,W=None,E=None,u=None,c=None,
+                  xlabel=None,head=None,foot=None):
+        self.input.u = self.input.u if u is None else u
+        self.input.c = self.input.c if c is None else c
+        self.mon.plot(self,i,j,v,W,E)
+        if xlabel is not None: self.mon.xlabel(j,xlabel)
+        if head is not None: self.mon.head(head)
+        if foot is not None: self.mon.foot(foot)
+
+    def set(self,u=None,c=None,x=None,y=None,b=None):
+        self.input.u = self.input.u if u is None else u
+        self.input.c = self.input.c if c is None else c
+        self.x = self.x if x is None else x
+        self.y = self.y if y is None else y
+        self.b = self.b if b is None else b
+        return self
 
 #===============================================================================
 # helper: toy cell parameters
