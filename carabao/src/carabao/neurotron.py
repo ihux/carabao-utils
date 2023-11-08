ca59e7e4c622dfe38f2e85c291606ba2af3eea69
iHux <hugo.pristauz@gmail.com>
2023-11-08 17:56:22 +0100

    ready for session

--- carabao/src/carabao/neurotron.py
+++ carabao/src/carabao/neurotron.py
@@ -67,15 +67,15 @@ class Pulse:
     """
     pulse: pulse unit
     >>> o = Pulse(lag=2,duty=3)             # mandatory args
-    >>> o = Pulse(lag=2,duty=3,log="P2/3")  # set log text
-    >>> y = o.feed(u:=1)                    # feed new input, receive new output
+    >>> o = Pulse(lag=2,duty=3,name='P2/3') # set name
+    >>> y = o(u:=1)                         # apply input, receive new output
     P2/3 1 -> ([1,0,0],0/3) -> 0
     >>> u = o.inp()                         # retrieve recent input
     >>> y = o.out()                         # get pulse output
     """
-    def __init__(self,lag,duty,log=None):
+    def __init__(self,lag,duty,name=None):
         def zeros(n): return [0 for k in range(0,n)]
-        self.head = log                # log header
+        self.name = name                # name header
         self.n = duty                  # duty = pulse length
         self.s = zeros(lag+1)          # shift register
         self.c = 0                     # counter
@@ -83,11 +83,17 @@ class Pulse:
     def feed(self,u):
         self.s = [u] + self.s[:-1]
         self.c = self.n if self.s[-1] > 0 else max(0,self.c-1)
-        if self.head is not None: print(self)
+        if self.name is not None: print(self)
         return self.out()
 
     def inp(self): return self.s[0]
     def out(self): return (self.c > 0) + 0
+
+    def __call__(self,u,log=None):
+        y = self.feed(u)
+        if log is not None:
+            print(log,self)
+        return y
 
     def __repr__(self):
         def string(l):
@@ -95,7 +101,8 @@ class Pulse:
             for i in range(0,len(l)): s += sep + "%g"%l[i]; sep = ','
             return s + ']'
         o = self;  body = "(%s,%g/%g)" % (string(o.s),o.c,o.n)
-        return o.head + " %g -> " % o.inp() + body +  " -> %g" % o.out()
+        name = o.name if o.name is not None else ""
+        return name + " %g -> " % o.inp() + body +  " -> %g" % o.out()
 
 #===================================================================================
 # class: Synapses
