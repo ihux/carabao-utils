c79def8363c4fca5d24ff4fd019ec8b42465bfb6
iHux <hugo.pristauz@gmail.com>
2023-11-23 16:06:07 +0000

    unit tests forTensor passed

--- carabao/src/carabao/matrix.py
+++ carabao/src/carabao/matrix.py
@@ -86,29 +86,225 @@ class Tensor:
     """
     class Tensor: implements a matrix of matrices (4-tensor)
     >>> T = Tensor(3,4,2,5)
-    Tensor(3,4,5,2)
+    >>> T.Kmap()
+    +-000/0-+-003/3-+-006/6-+-009/9-+
+    | 00000 | 00000 | 00000 | 00000 |
+    | 00000 | 00000 | 00000 | 00000 |
+    +-001/1-+-004/4-+-007/7-+-010/A-+
+    | 00000 | 00000 | 00000 | 00000 |
+    | 00000 | 00000 | 00000 | 00000 |
+    +-002/2-+-005/5-+-008/8-+-011/B-+
+    | 00000 | 00000 | 00000 | 00000 |
+    | 00000 | 00000 | 00000 | 00000 |
+    +-------+-------+-------+-------+
     >>> K = Matrix(2,5)
     >>> T = Tensor([[K,K,K,K],[K,K,K,K],[K,K,K,K]])
-    +-----+-----+-----+-----+
-    |00000|00000|00000|00000|
-    +-----+-----+-----+-----+
-    |00000|00000|00000|00000|
-    +-----+-----+-----+-----+
-    |00000|00000|00000|00000|
-    +-----+-----+-----+-----+
+    >>> T.Kmap()
+    +-000/0-+-003/3-+-006/6-+-009/9-+
+    | 00000 | 00000 | 00000 | 00000 |
+    | 00000 | 00000 | 00000 | 00000 |
+    +-001/1-+-004/4-+-007/7-+-010/A-+
+    | 00000 | 00000 | 00000 | 00000 |
+    | 00000 | 00000 | 00000 | 00000 |
+    +-002/2-+-005/5-+-008/8-+-011/B-+
+    | 00000 | 00000 | 00000 | 00000 |
+    | 00000 | 00000 | 00000 | 00000 |
+    +-------+-------+-------+-------+
     """
 
-    def __init__(self,arg,n=None,d=None,s=None):
-        if isinstance(arg,tuple):
-            assert len(arg.shape) == 2
+    def __init__(self,arg=None,n=None,d=None,s=None):
+        arg = 1 if arg is None else arg
+        if isinstance(arg,list):
+            assert len(arg) > 0
+            assert isinstance(arg[0],list) and len(arg[0]) > 0
             self.data = np.array(arg)
+            m = len(arg); n = len(arg[0])
+            d,s = self.data[0,0].shape
         else:
             m = arg
             if n is None: n = 1
             if d is None: d = 1
             if s is None: s = 1
-            list = [[Matrix(d,s) for j in range(n)] for i in range(m)]
-            self.data = array(list)
+            lst = [[Matrix(d,s) for j in range(n)] for i in range(m)]
+            self.data = np.array(lst)
+        self.shape = (m,n,d,s)
+
+    def kappa(self,i,j=None):
+        """
+        self.kappa():  convert matrix indices to linear index or vice versa
+        >>> Tensor(4,10).kappa(i:=1,j:=3)   # k = i + j*m
+        13
+        >>> Tensor(4,10).kappa(k:=13)       # i = k%m, j = k//m
+        (1, 3)
+        """
+
+        m,n,d,s = self.shape
+        if j is None:
+            k = i
+            return (k%m,k//m)
+        else:
+            return i + j*m
+
+    def permanence(self,p):    # encode permanence
+        """
+        self.permanence(p): convert permanence to symbolic string
+        >>> o = Tensor(1,1)
+        >>> o.permanence(0.52)
+        'B'
+        >>> o.permanence([-1,0,0.01,0.49,0.5,0.99,1,2])
+        '<0yaAY1>'
+        """
+        def upper(x):
+            return chr(int(65+(x-0.5)*100//2))
+        def lower(x):
+            return chr(int(65+32+(0.5-x)*100//2))
+
+        if isinstance(p,list):
+            s = ''
+            for k in range(len(p)):
+                s += self.permanence(p[k])
+            return s
+
+        if p < 0:
+            return '<'
+        elif p == 0:
+            return '0'
+        elif p == 1:
+            return '1'
+        elif p > 1:
+            return '>'
+        elif p < 0.5:
+            return lower(p)
+        elif p >= 0.5:
+            return upper(p)
+        else:
+            return '?'
+
+    def symbol(self,x):
+        """
+        self.symbol(x): convert index to symbol or vice versa
+        >>> o = Tensor(1,1)
+        >>> o.symbol(11)
+        'B'
+        >>> o.symbol([0,1,10,11,35,36,37,61,62])
+        '01ABZabz062'
+        """
+        def symb(x):
+            if x < 10:
+                return chr(48+x)
+            if x < 36:
+                return chr(55+x)
+            elif x < 62:
+                return chr(61+x)
+            else:
+                return '%03g' % x
+
+        if isinstance(x,int):
+            return symb(x)
+        elif isinstance(x,float):
+           return symb(int(x))
+        elif isinstance(x,list):
+            s = ''
+            for k in range(len(x)):
+                s += self.symbol(x[k])
+            return s
+
+    def bar(self,n,label='',k=-1):          # bar string of length n
+            if n >= 5:
+                if k >= 0:
+                    str = '%03g' % k
+                    if len(label) > 0:
+                        str += '/' + label
+                else:
+                    str = '---'
+                while len(str) < n:
+                    str += '-'
+                    if len(str) < n: str = '-' + str
+                return str
+            if n >= 3:
+                label = '-' + label
+            elif n >= 5:
+                label = '-' + label
+            str = label
+            for k in range(n-len(label)): str += '-'
+            return str
+
+    def head(self,i,n,s,width=0):
+        line = '+'
+        s = max(s,width)
+        for j in range(n):
+            if i < 0:
+                sym = ''
+                line += self.bar(s,'') + '+'
+            else:
+                k = self.kappa(i,j)
+                sym = self.symbol(k)
+                line += self.bar(s,sym,k) + '+'
+        return line
+
+    def _table(self,kind,I,m,n,width=0,label=''):    # print table
+        """
+        self.table('i',...) # for indices
+        self.table('p',...) # for permanences
+        self.table('w',...) # for synaptic weights
+        """
+        def title(n,x):
+            return '-%03g-' % x
+
+        def row(kind,I,i,j,d,s,width):
+            #return '12345'
+            str = ''
+            for nu in range(s):
+               if kind == 'i':   # index
+                   str += self.symbol(I[nu])
+               elif kind == 'p': # permanence
+                   str += self.permanence(I[nu])
+               elif kind == 'w': # permanence
+                   str += '1' if I[nu] > 0.5 else '0'
+               else:
+                   str += '?'
+            if kind == 's':
+                str = I
+            while len(str) < width:
+                str = str + ' '
+                if len(str) < width: str = ' ' + str
+            return str
+
+        #cells = self.cluster
+        d = len(I[0][0])
+        s = len(I[0][0][0])
+
+        tab = ''
+        for k in range(len(label)):
+            tab += ' '
+
+        str = ''
+        for i in range(m):
+            head = self.head(i,n,s,width)
+            trailer = label if i == 0 else tab
+            print(trailer+head)
+            for mu in range(d):
+                line = tab + '|'
+                for j in range(n):
+                    line += row(kind,I[i][j][mu],i,j,mu,s,width) + '|'
+                print(line)
+        print(tab+self.head(-1,n,s,width))
+
+    def Pmap(self):
+        m,n,d,s = self.shape
+        self._table('p',self.cluster.P,m,n,width=max(s,7),label='P: ')
+
+    def Kmap(self):
+        m,n,d,s = self.shape
+        self._table('i',self.data,m,n,width=max(s,7),label='')
+
+    def Gmap(self):
+        m,n,d,s = self.shape
+        self._table('i',self.cluster.G,m,n,width=max(s,7),label='')
+
+    def Wmap(self):
+        m,n,d,s = self.shape
+        self._table('w',self.cluster.P,m,n,width=max(s,7),label='')
 
 #===============================================================================
 # unit tests
@@ -154,9 +350,10 @@ def _case3():
 def _case4():
     """
     >>> T = Tensor(1)
-    +-+
-    |0|
-    +-+
+    >>> T.Kmap()
+    +-000/0-+
+    |   0   |
+    +-------+
     """
 
 #===============================================================================
